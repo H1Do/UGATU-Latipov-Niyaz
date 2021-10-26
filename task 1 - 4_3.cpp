589807229110005902276793181f97aa267e1f65
H1Do <90624145+H1Do@users.noreply.github.com>
2021-10-26 21:41:53 +0500

    Ready ready 4_3

--- task 1 - 4_3.cpp
+++ task 1 - 4_3.cpp
@@ -1,11 +1,11 @@
-// 55394311
+// 55485600
 // https://contest.yandex.ru/contest/29212/problems/4_3
 #include <cassert>
 #include <iostream>
 
 class Stack {
 public:
-  explicit Stack(int _buffer_size);
+  Stack();
   ~Stack();
 
   void push(int a);
@@ -13,27 +13,22 @@ public:
 
   bool empty() const { return top == -1; }
   int Size();
-  void BufferBoost();
 
 private:
   int *buffer;
   int buffer_size;
   int top;
+  void BufferBoost();
 };
 
-Stack::Stack(int _buffer_size) : buffer_size(_buffer_size), top(-1), buffer() {
-  buffer = new int[buffer_size];
-}
+Stack::Stack() : buffer_size(16), top(-1), buffer(new int[16]) {}
 
 Stack::~Stack() { delete[] buffer; }
 
 void Stack::push(int a) {
-  if (top < buffer_size - 1) {
-    buffer[++top] = a;
-  } else {
+  if (top >= buffer_size - 1)
     BufferBoost();
-    buffer[++top] = a;
-  }
+  buffer[++top] = a;
 }
 
 int Stack::pop() { return (top == -1) ? -1 : buffer[top--]; }
@@ -53,49 +48,46 @@ void Stack::BufferBoost() {
 
 class Queue {
 private:
-  Stack A = Stack(16);
-  Stack B = Stack(16);
+  Stack StackA;
+  Stack StackB;
 
 public:
-  void push(int el) { A.push(el); }
+  void push(int el) { StackA.push(el); }
 
   int pop() {
-    int size = A.Size();
-    if (B.empty())
+    int size = StackA.Size();
+    if (StackB.empty())
       for (int i = 0; i < size + 1; i++)
-        B.push(A.pop());
-    return B.pop();
+        StackB.push(StackA.pop());
+    return StackB.pop();
   }
 };
 
-bool testing(int In, int elem, Queue &A) {
-  if (In == 3) {
-    A.push(elem);
-  } else if (In == 2) {
-    if (A.pop() != elem) {
-      return false;
-    }
-  }
+bool testing(std::pair<int, int> Elem, Queue &Q) {
+  if (Elem.first == 3)
+    Q.push(Elem.second);
+  else if (Elem.first == 2 && Q.pop() != Elem.second)
+    return false;
   return true;
 }
 
 int main() {
-  Queue A;
+  Queue queue;
   bool flag = true;
 
   int k;
   std::cin >> k;
+  std::pair<int, int> Elem;
 
   for (int i = 0; i < k; i++) {
-    int In;
-    std::cin >> In;
-    int elem;
-    std::cin >> elem;
+    std::cin >> Elem.first >> Elem.second;
 
-    if (!testing(In, elem, A))
+    if (!testing(Elem, queue)) {
       flag = false;
+      break;
+    }
   }
 
-  std::cout << ((flag) ? "YES" : "NO") << '\n';
+  std::cout << (flag ? "YES" : "NO") << '\n';
   return 0;
 }
