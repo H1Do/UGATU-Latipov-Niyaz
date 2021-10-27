7abc57d84a99fcf512f8ea6cec63d0dd2132de1d
H1Do <90624145+H1Do@users.noreply.github.com>
2021-10-27 22:19:57 +0500

    Update 5_4
    Теперь проблем с TL нет

--- task 1 - 5_4.cpp
+++ task 1 - 5_4.cpp
@@ -1,8 +1,9 @@
-// 55488550
+// 55537941
 // https://contest.yandex.ru/contest/29212/problems/5_4/
 #include <iostream>
 #include <string>
 
+// Функция, которая выводит приоритет введенного оператора
 int Prior(char op) {
   if (op == '*' || op == '/')
     return 2;
@@ -11,17 +12,17 @@ int Prior(char op) {
   return -1;
 }
 
-class Stack {
+template <typename T> class Stack {
 public:
   Stack();
   ~Stack();
 
-  void push(int a);
-  int pop();
+  void push(T a);
+  T pop();
 
   bool empty() const { return top == -1; }
-  int Size();
-  int Top();
+  const int Size();
+  const T Top();
 
 private:
   int *buffer;
@@ -30,23 +31,26 @@ private:
   void BufferBoost();
 };
 
-Stack::Stack() : buffer_size(16), top(-1), buffer(new int[16]) {}
+template <typename T>
+Stack<T>::Stack() : buffer_size(16), top(-1), buffer(new int[16]) {}
 
-Stack::~Stack() { delete[] buffer; }
+template <typename T> Stack<T>::~Stack() { delete[] buffer; }
 
-void Stack::push(int a) {
+template <typename T> void Stack<T>::push(T a) {
   if (top >= buffer_size - 1)
     BufferBoost();
   buffer[++top] = a;
 }
 
-int Stack::pop() { return (top == -1) ? -1 : buffer[top--]; }
+template <typename T> T Stack<T>::pop() {
+  return (top == -1) ? -1 : buffer[top--];
+}
 
-int Stack::Top() { return buffer[top]; }
+template <typename T> const T Stack<T>::Top() { return buffer[top]; }
 
-int Stack::Size() { return top; }
+template <typename T> const int Stack<T>::Size() { return top; }
 
-void Stack::BufferBoost() {
+template <typename T> void Stack<T>::BufferBoost() {
   int new_buffer_size = buffer_size * 2;
   int *new_buffer = new int[new_buffer_size];
   for (int i = 0; i < buffer_size; i++) {
@@ -57,92 +61,128 @@ void Stack::BufferBoost() {
   buffer_size = new_buffer_size;
 }
 
-int Calculate(std::string Postfyx) {
+// Функция, вычисляющая постфиксное выражение
+int Calculate(std::string Postfix) {
+
   int first_int;
   int second_int;
   bool is_first_int = true;
-  Stack stack;
+  Stack<int> stack;
   int temp = 0;
 
-  for (int i = 0; i < (int)Postfyx.size(); i++) {
-    if (Postfyx[i] >= '0' && Postfyx[i] <= '9') {
+  for (int i = 0; i < Postfix.size(); i++) {
+
+    // Считывание числа из постфиксной записи
+    if (Postfix[i] >= '0' && Postfix[i] <= '9') {
       if (is_first_int) {
+
         is_first_int = false;
-        stack.push(Postfyx[i] - '0');
+        stack.push(Postfix[i] - '0');
+
       } else {
+
         temp = stack.pop();
-        stack.push(temp * 10 + Postfyx[i] - '0');
+        stack.push(temp * 10 + Postfix[i] - '0');
       }
+
+      // Считывание арифметических операторов
     } else {
-      if (Postfyx[i] == ' ')
+
+      if (Postfix[i] == ' ')
         is_first_int = true;
+
       else {
         first_int = stack.pop();
         second_int = stack.pop();
-        if (Postfyx[i] == '+') {
+
+        // Операция над числами в зависимости от оператора
+        if (Postfix[i] == '+') {
           stack.push(second_int + first_int);
-        } else if (Postfyx[i] == '-') {
+        } else if (Postfix[i] == '-') {
           stack.push(second_int - first_int);
-        } else if (Postfyx[i] == '/') {
+        } else if (Postfix[i] == '/') {
           stack.push(second_int / first_int);
-        } else if (Postfyx[i] == '*') {
+        } else if (Postfix[i] == '*') {
           stack.push(second_int * first_int);
         }
       }
     }
   }
-  return stack.Top();
+
+  // Возвращаем результат
+  return stack.pop();
 }
 
-void inf_to_post(std::string &Postfyx, Stack &stack) {
+// Функция, преобразующая инфиксную форму записи в постфиксную
+void inf_to_post(std::string &Postfix, Stack<char> &stack) {
   bool is_int = true;
   char el;
 
-  while ((el = getchar()) && (el != '\n')) {
-    if (el >= '0' && el <= '9') {
-      is_int = true;
-      Postfyx.push_back(el);
-    } else {
-      if (is_int) {
-        Postfyx.push_back(' ');
-        is_int = false;
+  while (std::cin >> el && el != '\n') {
+      // Если встретилось число, помещаем его в результирующую строку
+      if (el >= '0' && el <= '9') {
+        is_int = true;
+        Postfix.push_back(el);
       }
-      if (el == '(' || el == ')') {
-        if (el == '(') {
-          stack.push(el);
-        } else {
-          while (stack.Top() != '(') {
-            Postfyx.push_back(char(stack.pop()));
-          }
-          stack.pop();
+
+      // Когда число закончилось, ставим после него пробел
+      else {
+        if (is_int) {
+          Postfix.push_back(' ');
+          is_int = false;
         }
-      } else {
-        if (stack.empty()) {
-          stack.push(el);
-        } else {
-          while (!stack.empty()) {
-            if (Prior(char(stack.Top())) >= Prior(el)) {
-              Postfyx.push_back(char(stack.pop()));
-            } else {
-              break;
+
+        // Встретив левую скобку, помещаем её в стэк
+        // Встретив правую - перекидываем элементы со стэка в результирующую
+        // строку, пока не стретим левую скобку
+        if (el == '(' || el == ')') {
+          if (el == '(') {
+            stack.push(el);
+          } else {
+            while (stack.Top() != '(') {
+              Postfix.push_back(stack.pop());
             }
+            stack.pop();
           }
-          stack.push(el);
+        }
+
+        // Встречаем оператор
+        else {
+
+          // Если стэк пустой, помещаем оператор в него
+          if (stack.empty()) {
+            stack.push(el);
+          }
+
+          // Если стэк не пустой, перемещаем элементы из него, пока не
+          // встретиться оператор с меньшим приоритетом, чем считанный со строки
+          else {
+            while (!stack.empty()) {
+              if (Prior(stack.Top()) >= Prior(el)) {
+                Postfix.push_back(stack.pop());
+              } else {
+                break;
+              }
+            }
+            stack.push(el);
+          }
         }
       }
     }
-  }
+
   while (!stack.empty()) {
-    Postfyx.push_back(char(stack.pop()));
+    Postfix.push_back(stack.pop());
   }
 }
 
 int main() {
   std::string Postfix;
-  Stack stack;
+  Stack<char> stack;
 
+  // Считываем и преобразуем инфиксную форму запись в постфиксную
   inf_to_post(Postfix, stack);
 
-  std::cout << Calculate(Postfix);
+  // Вычисляем постфиксное выражение
+  std::cout << Calculate(Postfix) << std::endl;
   return 0;
 }
