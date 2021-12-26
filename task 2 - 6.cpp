b270ab28a8324266ac228f786e83bdbf7ae5f8b6
H1Do <90624145+H1Do@users.noreply.github.com>
2021-12-26 19:14:05 +0500

    Update task 2 - 6.cpp

--- task 2 - 6.cpp
+++ task 2 - 6.cpp
@@ -1,10 +1,9 @@
-// 59015475 TL, а в моих тестах ошибка, причем по отдельности сортировки работают
-// исправно. Если есть такая возможность, можете помочь
+// 63116316
 // https://contest.yandex.ru/contest/30914/problems/6
+#include "sort.h"
 #include <stack>
 
-#include "sort.h"
-
+// Сортировка вставками
 void InsertionSort(unsigned int* arr, int n) {
   for (int i = 1; i < n; i++) {
     unsigned int tmp = arr[i];
@@ -15,56 +14,59 @@ void InsertionSort(unsigned int* arr, int n) {
 }
 
 // Поиск медианы первого, последнего и среднего числа массива
-int Pivot(unsigned int* arr, size_t left, size_t right) {
-  return (arr[left] + arr[right] + arr[(left + right) / 2]) / 3;
+size_t Pivot(unsigned int* arr, size_t left, size_t right) {
+  size_t mid = (left + right) / 2;
+
+  if (arr[left] > arr[right]) std::swap(arr[left], arr[right]);
+  if (arr[left] > arr[mid]) std::swap(arr[left], arr[mid]);
+  if (arr[mid] > arr[right]) std::swap(arr[mid], arr[right]);
+
+  return mid;
 }
 
 // Разбиение Хоара
 unsigned int Partition(unsigned int* arr, size_t left, size_t right) {
-  if (left == right) return left;
-  unsigned int pivot = Pivot(arr, left, right);
+  if (left < right - 2) std::swap(arr[Pivot(arr, left, right)], arr[right]);
 
   size_t i = left;
   size_t j = right - 1;
 
   while (i <= j) {
-    while (arr[i] < pivot) i++;
-    while (arr[j] > pivot) j--;
+    while (arr[i] < arr[right]) i++;
+    while (arr[j] > arr[right]) j--;
     if (i <= j) {
-      std::swap(arr[i], arr[j]);
-      i++;
-      j--;
+      std::swap(arr[i++], arr[j--]);
     }
   }
+  std::swap(arr[i], arr[right]);
   return i;
 }
 
+// Быстрая сортировка
 void QuickSort(unsigned int* arr, size_t left, size_t right) {
-  std::stack<std::pair<unsigned int, unsigned int>> stk;
-  std::pair<unsigned int, unsigned int> pr{left, right};
-  stk.push(pr);
+  std::stack<std::pair<unsigned int, unsigned int>> bounds_stack;
+  std::pair<unsigned int, unsigned int> bounds{left, right};
+  bounds_stack.push(bounds);
 
-  while (!stk.empty()) {
-    pr = stk.top();
-    stk.pop();
+  while (!bounds_stack.empty()) {
+    bounds = bounds_stack.top();
+    bounds_stack.pop();
 
-    size_t left = pr.first;
-    size_t right = pr.second;
+    size_t left = bounds.first;
+    size_t right = bounds.second;
 
     if (right - left <= 30) {
       InsertionSort(arr + left, right - left + 1);
     } else {
-      int i = Partition(arr, left, right);
+      unsigned int i = Partition(arr, left, right);
 
       if (i > left + 1) {
-        pr.first = left;
-        pr.second = i - 1;
-        stk.push(pr);
+        bounds = {left, i - 1};
+        bounds_stack.push(bounds);
       }
       if (i < right - 1) {
-        pr.first = i + 1;
-        pr.second = right;
-        stk.push(pr);
+        bounds = {i + 1, right};
+        bounds_stack.push(bounds);
       }
     }
   }
