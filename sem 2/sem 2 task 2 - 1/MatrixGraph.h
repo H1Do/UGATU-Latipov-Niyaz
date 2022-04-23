c9bb4671d8c30ec7ee3425bd3f3cdf2ba0d55a84
H1Do <90624145+H1Do@users.noreply.github.com>
2022-04-23 17:02:34 +0500

    .

---
+++ sem 2/sem 2 task 2 - 1/MatrixGraph.h
@@ -0,0 +1,25 @@
+//
+// Created by HidoU on 19.04.2022.
+//
+
+#ifndef MATRIXGRAPH_H_
+#define MATRIXGRAPH_H_
+
+#include "IGraph.h"
+
+class MatrixGraph : IGraph {
+ public:
+  using Matrix = std::vector<std::vector<int>>;
+  explicit MatrixGraph(int size);
+  explicit MatrixGraph(const IGraph* graph);
+  void AddEdge(int from, int to) override;
+  int VerticesCount() const override;
+  void FindAllAdjacentIn(int vertex, std::vector<int>& vertices) const override;
+  void FindAllAdjacentOut(int vertex, std::vector<int>& vertices) const override;
+
+ private:
+  Matrix graph;
+  int size;
+};
+
+#endif //MATRIXGRAPH_H_
